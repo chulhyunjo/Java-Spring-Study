d064bd0568aba41768109c9a3721a88a70b695c5
조은비 <mathcom128@gmail.com>
2022-12-11 23:05:08 +0900

    조은비/22/12/11/spring기초

--- eunbee/spring/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ eunbee/spring/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -11,5 +13,39 @@ public class HelloController {
     public String hello(Model model){
         model.addAttribute("data", "hello!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String hello(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody // 그대로 데이터를 보내줌
+    public String helloString(@RequestParam("name") String name){
+        return "hello"+name;
+    }
+
+    @GetMapping("hello-api") //JSON방식
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+
+
+    static class Hello{
+        private String name;
+
+        public String getName(){
+            return name;
+        }
+
+        public void setName(String name){
+            this.name = name;
+        }
     }
 }
