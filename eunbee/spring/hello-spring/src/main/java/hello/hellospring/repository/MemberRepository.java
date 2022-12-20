3a502e95407f149cfbcde0b9771ba7311f3f3788
조은비 <mathcom128@gmail.com>
2022-12-20 20:10:31 +0900

    조은비/22/12/20/spring공부

--- eunbee/spring/hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
+++ eunbee/spring/hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -2,8 +2,10 @@ package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
 
+
 import java.util.List;
 import java.util.Optional;
+
 
 public interface MemberRepository{
     Member save(Member member);
--- eunbee/spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ eunbee/spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -3,14 +3,19 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 import java.util.Optional;
 
+@Service //component라는 어노테이션이 있음
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
     // dependency Injection (DI) 의존관계 주입
+    @Autowired
     public MemberService(MemberRepository memberRepository){
         this.memberRepository = memberRepository;
     }
